f16660fcf2d6eb6f646dcccb633f84ecb44a08ce
Carlos181312 <crlsabm18@gmail.com>
2026-01-29 12:00:26 -0600

    Actualización 28Ene26

--- 02-Fundamentos Python/07-tupples.py
+++ 02-Fundamentos Python/07-tupples.py
@@ -1,3 +1,10 @@
 my_tuple = (1, 2, 3, 4, "Hola", True, False, 2, "hi", 3)
 
+#print(my_tuple.count(2))
+#print(my_tuple.index(2))3
+
+my_tuple[1] = 20
+
 print(my_tuple)
+
+week = ("Lunes", "Martes", "Miercoles")
